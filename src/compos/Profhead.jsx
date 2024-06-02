a063b94245afd89cf7f657c5495564cb11150d1c
webb-orbit <webbenemies@gmail.com>
2024-06-02 20:43:52 +0530

    conform you reboun

--- src/compos/Profhead.jsx
+++ src/compos/Profhead.jsx
@@ -18,7 +18,7 @@ const [datas, setdatas] = useState([])
     return datas? (
         <>
             <div className=' w-full bg-transparent text-gray-200 h-[15vh] flex items-start justify-center flex-col px-20 poppins-regular tracking-[1.5px] max-[800px]:h-[20vh] max-sm:px-6 max-sm:h-[5rem]'>
-                <h1 className=' capitalize font-semibold text-[2rem] select-none max-[800px]:text-[5vw] text-nowrap overflow-x-hidden w-[90%]'>wellcome {datas.name}</h1>
+                <h1 className=' capitalize font-semibold text-[2rem] select-none max-[800px]:text-[5vw] text-nowrap overflow-x-hidden w-[90%]'>{datas.name}</h1>
                 <p className=' ml-4 text-[0.6rem] max-[800px]:text-[2vw] max-[800px]:ml-1'>ID: {datas.$id}</p>
             </div>
         </>
