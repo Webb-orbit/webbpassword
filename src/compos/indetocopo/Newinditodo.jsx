90a66129f1d9b695a27159e1840ae31bce01b3ef
webb-orbit <webbenemies@gmail.com>
2024-06-07 18:08:36 +0530

    nothing

--- src/compos/indetocopo/Newinditodo.jsx
+++ src/compos/indetocopo/Newinditodo.jsx
@@ -5,36 +5,39 @@ import "../../editor.css"
 const Newinditodo = () => {
   const [plain, setplain] = useState("")
   const textref = useRef(null)
+  
+  function escapee(str) {
+    return str.replace(/</g, '&lt;')
+              .replace(/>/g, '&gt;')
+              .replace(/"/g, '&quot;')
+              .replace(/'/g, '&#039;');
+}
 
   const texttomd = ()=>{
     let htm = plain
-    .replace(/\((.*?)\)\[(.*?)\]/gim, "<a href=$1>$2</a>")
-
-    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
-    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
-    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
+    .replace(/\((.*?)\)\[(.*?)\]/gim, "<a data-md='not' target='_blank' href='$1'>$2</a>")
     
-    .replace(/^\*\*(.*$)/gim, "<ul><li>$1</li></ul>")
+    .replace(/^# (.*$)/gim, "<h1 data-md='not'>$1</h1>")
+    .replace(/^## (.*$)/gim, "<h2 data-md='not'>$1</h2>")
+    .replace(/^### (.*$)/gim, "<h3 data-md='not'>$1</h3>")
+    
+    .replace(/^\*\*(.*$)/gim, "<ul data-md='not'><li>$1</li></ul>")
     .replace(/<\/ul>\s*<ul>/gim, "")
-
-    .replace(/^\d\.(.*$)/gim, "<ol><li>$1</li></ol>")
+    
+    .replace(/^\d\.(.*$)/gim, "<ol data-md='not'><li>$1</li></ol>")
     .replace(/<\/ol>\s*<ol>/gim, "")
-
-    .replace(/\*(.*)\*/gim, "<b>$1</b>")
-
-    .replace(/^>(.*$)/gim, '<blockquote>$1</blockquote>') 
+    
+    .replace(/\*(.*)\*/gim, "<b data-md='not'>$1</b>")
+    
+    .replace(/^>(.*$)/gim, '<blockquote data-md="not">$1</blockquote>') 
     .replace(/<\/blockquote>\s*<blockquote>/gim, '<br/>')
+    
+    .replace(/`([\s\S]*?)`/gim, (_, p1) => `<pre data-md='not' ><code>${escapee(p1)}</code></pre>`)
+    
+    .replace(/^---/gm, "<hr data-md='not' />")
+    .replace(/\n/gim, "<br data-md='not' />")
 
-    .replace(/`([\s\S]*?)`/gim, '<pre><code>$1</code></pre>')
-    .replace(/`(.*?)`/gim, '<code>$1</code>')
-    
-    .replace(/^---/gm, "<hr/>")
-
-    .replace(/\n/gim, "<br/>")
-    
-    
     textref.current.innerHTML = htm
-    console.log(textref.current.ariaRowIndex);
   }
 
   useEffect(()=>{
@@ -42,10 +45,10 @@ const Newinditodo = () => {
   },[plain])
     
   return (
-    <div className="flex min-h-[10rem]  h-full justify-around poppins-regular items-stretch">
-      <textarea onChange={(e)=>setplain(e.target.value)} className=" resize-none p-5 w-[45%] bg-black  "></textarea>
+    <div className="flex min-h-[10rem] h-full justify-around poppins-regular items-stretch">
+      <textarea spellCheck="false" onChange={(e)=>setplain(e.target.value)} className=" resize-none p-5 w-[45%] bg-black  "></textarea>
 
-      <div ref={textref} className=" p-5 w-[45%] bg-[#242424]  "></div>
+      <div ref={textref} className=" whitespace-pre-wrap break-words p-5 w-[45%] bg-[#242424]"></div>
     </div>
   )
 }
